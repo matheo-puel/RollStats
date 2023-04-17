c57887979481d649f792aab592754a1ab831c5de
Mathéo Puel <91758608+matheo-puel@users.noreply.github.com>
2023-04-17 12:49:06 +0200

    Update RollStats.py

--- RollStats.py
+++ RollStats.py
@@ -16,18 +16,12 @@ for i in range(X):
     Loading = (i/X)*100
     print(round(Loading, 2), "%")
     sys.stdout.write("\033[F")
-    if(R == 1):
-        liste1.append(R)
-    elif(R==2):
-        liste2.append(R)
-    elif(R==3):
-        liste3.append(R)
-    elif(R==4):
-        liste4.append(R)
-    elif(R==5):
-        liste5.append(R)
-    elif(R==6):
-        liste6.append(R)
+    if(R == 1): liste1.append(R) 
+    elif(R==2): liste2.append(R)
+    elif(R==3): liste3.append(R)
+    elif(R==4): liste4.append(R)
+    elif(R==5): liste5.append(R)
+    elif(R==6): liste6.append(R)
 
 l1 = len(liste1)
 l2 = len(liste2)
